8a392a989f6884c1ae0c37d6a0ce43bf26eb6ebb
roseleemarie <206959521+Roseleemarie@users.noreply.github.com>
2025-05-30 18:40:37 -0500

    add color on hover

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let btnCont = document.querySelector("#btnCont");
 let gridCont = document.querySelector("#gridCont");
 let button = document.querySelectorAll("button");
+let gridBox = document.createElement("div");
 
-size = 16
+
+let changecolor = "black";
+
+size = 25
+
+
 sizeVal=(600/size);
     for (let i=0;i<size*size;i++){
-        let gridBox = document.createElement("div");
+        let gridBox = document.createElement("div")
         gridBox.className = 'gridBox'
         gridBox.setAttribute("style", 
-            ` width: ${sizeVal}px;
+            `width: ${sizeVal}px;
             height: ${sizeVal}px;`);
+        gridBox.addEventListener("mouseenter", function () {
+            gridBox.style.background = 'black';
+        });
         gridCont.appendChild(gridBox);
     };
+
+
+
+
