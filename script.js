d7b5cbff6b291559008ef65f4cf7d7285013603d
roseleemarie <206959521+Roseleemarie@users.noreply.github.com>
2025-06-01 10:05:52 -0500

    add working size and clear buttons

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 let btnCont = document.querySelector("#btnCont");
 let gridCont = document.querySelector("#gridCont");
-let button = document.querySelectorAll("button");
-
-size = 16
-sizeVal=(600/size);
-    for (let i=0;i<size*size;i++){
-        let gridBox = document.createElement("div");
+//let gridBox = document.createElement("div");
+let changecolor = "black";
+size(16);
+function size (currentSize){
+sizeVal=(600/currentSize);
+    for (let i=0;i<currentSize*currentSize;i++){
+        let gridBox = document.createElement("div")
         gridBox.className = 'gridBox'
         gridBox.setAttribute("style", 
-            ` width: ${sizeVal}px;
+            `width: ${sizeVal}px;
             height: ${sizeVal}px;`);
+        gridBox.addEventListener("mouseenter", function () {
+            gridBox.style.background = "black";
+        });
         gridCont.appendChild(gridBox);
     };
+};
+let clearBtn = document.querySelector("#clear");
+
+clearBtn.addEventListener('click', ()=> gridClear('16'));
+function gridClear (currentSize){
+    let gridBoxArray = document.querySelectorAll(".gridBox");
+    [...gridBoxArray].forEach( (gridBox) => {
+        gridCont.removeChild(gridBox);
+    });
+    return size(currentSize);
+};
+let resizeBtn = document.querySelector("#size")
+resizeBtn.addEventListener('click',() => gridClear(""));
+resizeBtn.addEventListener('click',() => {
+    currentSize = prompt ("Enter a number from 2-100");
+    currentSize = parseInt(currentSize);
+    if (currentSize>=2 && currentSize<=100 && typeof(currentSize)==='number'){
+        return size(currentSize);
+    }   else {
+        alert("Not a valid value!");
+    };
+});
+
+
